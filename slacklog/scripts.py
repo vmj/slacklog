c1011d858706eebf65e18605bf1ddd793f6031f2
Mikko Värri <vmj@linuxbox.fi>
2017-04-05 14:51:17 +0300

    Move script code to a function

--- slacklog/scripts.py
+++ slacklog/scripts.py
@@ -113,3 +113,195 @@ def main(**kwargs):
         optionParser.error('Missing required option(s): %s' % ', '.join(missing))
 
     return opts, args
+
+
+def slacklog2atom():
+    #
+    #   Define and handle command line options
+    #
+    (opts, args) = main(
+        description='Convert Slackware ChangeLog to Atom',
+        options={
+            'changelog': {'help': 'Read input from FILE',
+                          'metavar': 'FILE', 'mandatory': True},
+            'encoding': {'help': 'ChangeLog encoding [default: %default]',
+                         'default': 'iso8859-1'},
+            'min-date': {'help': 'Last date to include [default: include all]',
+                         'metavar': 'DATE'},
+            'out': {'help': 'Write output to FILE',
+                    'metavar': 'FILE', 'mandatory': True},
+            'quiet': {'help': 'Do not print warnings',
+                      'action': 'store_true'},
+            'max-entries': {'help': 'Max number of Atom entries [default: infinity]',
+                            'metavar': 'NUM'},
+            'slackware': {'help': 'Slackware version [default: %default].',
+                          'default': 'Slackware 13.1'},
+            'link': {'help': 'Full URL of the Atom feed',
+                     'metavar': 'URL', 'mandatory': True},
+            'webLink': {'help': 'Full URL of the HTML version',
+                        'metavar': 'URL'},
+            'name': {'help': 'NAME of the feed author',
+                     'metavar': 'NAME'},
+            'email': {'help': 'EMAIL of the feed author',
+                      'metavar': 'EMAIL'}
+        })
+
+    #
+    #   Apply options to parser and formatter
+    #
+    parsers.SlackLogParser.quiet = opts.quiet
+    parsers.SlackLogParser.min_date = parsers.SlackLogParser.parse_date(u(opts.min_date))
+
+    formatters.SlackLogAtomFormatter.max_entries = i(opts.max_entries)
+    formatters.SlackLogAtomFormatter.slackware = u(opts.slackware)
+    formatters.SlackLogAtomFormatter.link = u(opts.link)
+    formatters.SlackLogAtomFormatter.webLink = u(opts.webLink)
+    formatters.SlackLogAtomFormatter.name = u(opts.name)
+    formatters.SlackLogAtomFormatter.email = u(opts.email)
+
+    #
+    #   Read input
+    #
+    txt = read(opts.changelog, opts.encoding)
+
+    #
+    #   Format output
+    #
+    atom = formatters.SlackLogAtomFormatter.format(parsers.SlackLogParser.parse(txt))
+
+    #
+    #   Write output
+    #
+    write(opts.out, atom)
+
+
+def slacklog2pyblosxom():
+    #
+    #   Define and handle command line options
+    #
+    (opts, args) = main(
+        description='Convert Slackware ChangeLog to PyBlosxom blog entries',
+        options={
+            'changelog': {'help': 'Read input from FILE',
+                          'metavar': 'FILE', 'mandatory': True},
+            'encoding': {'help': 'ChangeLog encoding [default: %default]',
+                         'default': 'iso8859-1'},
+            'min-date': {'help': 'Last date to include [default: include all]',
+                         'metavar': 'DATE'},
+            'datadir': {'help': 'PyBlosxom blog datadir',
+                        'metavar': 'DATADIR', 'mandatory': True},
+            'quiet': {'help': 'Do not print warnings',
+                      'action': 'store_true'},
+            'max-entries': {'help': 'Max number of blog entries [default: infinity]',
+                            'metavar': 'NUM'},
+            'slackware': {'help': 'Slackware version [default: %default].',
+                          'default': 'Slackware 13.1'},
+            'entry-extension': {'help': 'PyBlosxom entry extention [default: %default]',
+                                'default': 'txt'},
+            'entry-encoding': {'help': 'PyBlosxom entry encoding [default: %default]',
+                               'default': 'utf-8'},
+            'tags-separator': {'help': 'PyBlosxom tags separator [default: %default]',
+                               'default': ','},
+            'pkg-separator': {'help': 'Pkg description separator [default: %default]',
+                              'default': ':'},
+            'pyfilemtime': {'help': 'Enable pyfilemtime compliance',
+                            'action': 'store_true'},
+            'overwrite': {'help': 'Overwrite entries that exist',
+                          'action': 'store_true'},
+            'no-backup': {'help': 'Make a backup before overwriting',
+                          'action': 'store_true'},
+        })
+
+    #
+    #   Apply options to parser and formatter
+    #
+    parsers.SlackLogParser.quiet = opts.quiet
+    parsers.SlackLogParser.min_date = parsers.SlackLogParser.parse_date(u(opts.min_date))
+
+    formatters.SlackLogPyblosxomFormatter.max_entries = i(opts.max_entries)
+    formatters.SlackLogPyblosxomFormatter.quiet = opts.quiet
+    formatters.SlackLogPyblosxomFormatter.slackware = u(opts.slackware)
+    formatters.SlackLogPyblosxomFormatter.datadir = u(opts.datadir)
+    formatters.SlackLogPyblosxomFormatter.extension = u(opts.entry_extension)
+    formatters.SlackLogPyblosxomFormatter.encoding = u(opts.entry_encoding)
+    formatters.SlackLogPyblosxomFormatter.tags_separator = u(opts.tags_separator)
+    formatters.SlackLogPyblosxomFormatter.pkg_separator = u(opts.pkg_separator)
+    formatters.SlackLogPyblosxomFormatter.overwrite = opts.overwrite
+    formatters.SlackLogPyblosxomFormatter.backup = not opts.no_backup
+    formatters.SlackLogPyblosxomFormatter.pyfilemtime = opts.pyfilemtime
+
+    #
+    #   Read input
+    #
+    txt = read(opts.changelog, opts.encoding)
+
+    #
+    #   Format output (output goes to files, no need to capture the result)
+    #
+    formatters.SlackLogPyblosxomFormatter.format(parsers.SlackLogParser.parse(txt))
+
+
+def slacklog2rss():
+    #
+    #   Define and handle command line options
+    #
+    (opts, args) = main(
+        description='Convert Slackware ChangeLog to RSS',
+        options={
+            'changelog': {'help': 'Read input from FILE',
+                          'metavar': 'FILE', 'mandatory': True},
+            'encoding': {'help': 'ChangeLog encoding [default: %default]',
+                         'default': 'iso8859-1'},
+            'min-date': {'help': 'Last date to include [default: include all]',
+                         'metavar': 'DATE'},
+            'out': {'help': 'Write output to FILE',
+                    'metavar': 'FILE', 'mandatory': True},
+            'quiet': {'help': 'Do not print warnings',
+                      'action': 'store_true'},
+            'max-entries': {'help': 'Max number of RSS entries [default: infinity]',
+                            'metavar': 'NUM'},
+            'slackware': {'help': 'Slackware version [default: %default].',
+                          'default': 'Slackware 13.1'},
+            'rssLink': {'help': 'Full URL of the RSS feed',
+                        'metavar': 'URL', 'mandatory': True},
+            'webLink': {'help': 'Full URL of the HTML version',
+                        'metavar': 'URL'},
+            'description': {'help': 'Description of the RSS feed',
+                            'metavar': 'DESC'},
+            'language': {'help': 'Language of the RSS feed [default: %default]',
+                         'default': 'en'},
+            'managingEditor': {'help': 'EMAIL, and possibly NAME, of the editor',
+                               'metavar': 'EMAIL (NAME)'},
+            'webMaster': {'help': 'EMAIL, and possibly NAME, of the web master',
+                          'metavar': 'EMAIL (NAME)'}
+        })
+
+    #
+    #   Apply options to parser and formatter
+    #
+    parsers.SlackLogParser.quiet = opts.quiet
+    parsers.SlackLogParser.min_date = parsers.SlackLogParser.parse_date(u(opts.min_date))
+
+    formatters.SlackLogRssFormatter.max_entries = i(opts.max_entries)
+    formatters.SlackLogRssFormatter.slackware = u(opts.slackware)
+    formatters.SlackLogRssFormatter.rssLink = u(opts.rssLink)
+    formatters.SlackLogRssFormatter.webLink = u(opts.webLink)
+    formatters.SlackLogRssFormatter.description = u(opts.description)
+    formatters.SlackLogRssFormatter.language = u(opts.language)
+    formatters.SlackLogRssFormatter.managingEditor = u(opts.managingEditor)
+    formatters.SlackLogRssFormatter.webMaster = u(opts.webMaster)
+
+    #
+    #   Read input
+    #
+    txt = read(opts.changelog, opts.encoding)
+
+    #
+    #   Format output
+    #
+    rss = formatters.SlackLogRssFormatter.format(parsers.SlackLogParser.parse(txt))
+
+    #
+    #   Write output
+    #
+    write(opts.out, rss)
