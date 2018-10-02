6c981758dbda09d57fcade82e4a5e6e7553989af
Mikko Värri <vmj@linuxbox.fi>
2018-10-02 13:32:33 +0300

    Remove use of deprecated assertEquals

--- test/ParserTests.py
+++ test/ParserTests.py
@@ -14,7 +14,7 @@ class ParserTests (unittest.TestCase):
         self.assertTrue(log is not None)
         # The content starts and ends with entry separators.
         # The "empty" entries are ignored.
-        self.assertEquals(len(log.entries), 1)
+        self.assertEqual(len(log.entries), 1)
         e = log.entries[0]
         self.assertEqual(e.timestamp, datetime(2011, 4, 25, 13, 37, 00, tzinfo=tz.tzutc()))
         self.assertTrue(e.description.startswith(u'Slackware 13.37 x86 stable is released!\n\n'))
@@ -27,23 +27,23 @@ class ParserTests (unittest.TestCase):
     def test_slackware_leet_rc3_entry(self):
         log = SlackLogParser().parse(read('./test/slackware-leet-rc3-entry.txt', 'iso8859-1'))
         self.assertTrue(log is not None)
-        self.assertEquals(len(log.entries), 1)
+        self.assertEqual(len(log.entries), 1)
         e = log.entries[0]
         self.assertEqual(e.timestamp, datetime(2011, 3, 27, 8, 28, 47, tzinfo=tz.tzutc()))
         # Of special interest here is the line that starts with "candidate:  " which might be
         # confused with a package name.
-        self.assertEquals(e.description, u"""There have been quite a few changes so we will have one more release
+        self.assertEqual(e.description, u"""There have been quite a few changes so we will have one more release
 candidate:  Slackware 13.37 RC 3.14159265358979323846264338327950288419716.
 Very close now!  But we'll likely hold out for 2.6.37.6.
 """)
-        self.assertEquals(len(e.pkgs), 62)
-        self.assertEquals(e.pkgs[0].pkg, u'a/aaa_base-13.37-i486-3.txz')
+        self.assertEqual(len(e.pkgs), 62)
+        self.assertEqual(e.pkgs[0].pkg, u'a/aaa_base-13.37-i486-3.txz')
         # There are a few non-typical entries at the end
-        self.assertEquals(e.pkgs[57].pkg, u'extra/linux-2.6.37.5-nosmp-sdk/*')
-        self.assertEquals(e.pkgs[58].pkg, u'isolinux/initrd.img')
-        self.assertEquals(e.pkgs[59].pkg, u'kernels/*')
-        self.assertEquals(e.pkgs[60].pkg, u'usb-and-pxe-installers/usbboot.img')
-        self.assertEquals(e.pkgs[61].pkg, u'testing/source/linux-2.6.38.1-configs/')
+        self.assertEqual(e.pkgs[57].pkg, u'extra/linux-2.6.37.5-nosmp-sdk/*')
+        self.assertEqual(e.pkgs[58].pkg, u'isolinux/initrd.img')
+        self.assertEqual(e.pkgs[59].pkg, u'kernels/*')
+        self.assertEqual(e.pkgs[60].pkg, u'usb-and-pxe-installers/usbboot.img')
+        self.assertEqual(e.pkgs[61].pkg, u'testing/source/linux-2.6.38.1-configs/')
 
     def test_good_11(self):
         # these logs have a the same change in both of them
